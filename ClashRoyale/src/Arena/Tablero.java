60965c001c94b6105b98a86e6acac7608d2b32a9
javierfrances <javierfrances@usuario-OptiPlex-3050>
2020-02-18 14:29:33 +0100

    hola

--- ClashRoyale/src/Arena/Tablero.java
+++ ClashRoyale/src/Arena/Tablero.java
@@ -1,6 +1,6 @@
-package ClashRoyale;
+package Arena;
 
-public class Arena {
+public class Tablero {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo2222");
--- ClashRoyale/src/ClashRoyale/Jugadores.java
+++
@@ -1,5 +0,0 @@
-package ClashRoyale;
-
-public class Jugadores {
-
-}
