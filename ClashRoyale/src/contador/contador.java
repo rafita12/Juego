35af43b6e94079ab63084b542cf0aacc625bd7b6
javierfrances <javierfrances@usuario-OptiPlex-3050>
2020-02-25 14:27:22 +0100

    contador

--- ClashRoyale/src/contador/contador.java
+++ ClashRoyale/src/contador/contador.java
@@ -1,12 +1,148 @@
 package contador;
 
-public class contador {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-	private int tiempo;
-	private int tiempoExtra;
-	
-	public void tiempoAgotado() {
-		
-	}
-	
-}
+public class contador extends JFrame {
+
+    private static final long serialVersionUID = 3545053785228009472L;
+
+    private JPanel panel;
+    private JLabel timeLabel;
+
+    private JPanel buttonPanel;
+    private JButton startButton;
+    private JButton resetButton;
+    private JButton stopButton;
+
+    // Se inicializa el tiempo
+    private byte centiseconds = 0;
+    private byte seconds = 30;
+    private short minutes = 2;
+
+    private DecimalFormat timeFormatter;
+
+    private Timer timer;
+
+    public contador() {
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Consolas", Font.PLAIN, 13));
+        timeLabel.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(timeLabel);
+
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        startButton = new JButton("Iniciar Partida");
+        startButton.addActionListener(new ActionListener() {
+            
+        //Iniciar Partida
+        	public void actionPerformed(ActionEvent e) {
+
+                timer.start();
+
+            }
+        });
+        buttonPanel.add(startButton);
+
+        resetButton = new JButton("Reset");
+        resetButton.addActionListener(new ActionListener() {
+            
+        	//Resetear contador
+        	public void actionPerformed(ActionEvent e) {
+
+                timer.stop();
+
+                centiseconds = 0;
+                seconds = 30;
+                minutes = 2;
+
+                timeLabel.setText(timeFormatter.format(minutes) + ":"
+                        + timeFormatter.format(seconds) + "."
+                        + timeFormatter.format(centiseconds));
+            }
+        });
+
+        buttonPanel.add(resetButton);
+
+        stopButton = new JButton("Stop");
+        stopButton.addActionListener(new ActionListener() {
+           
+        	
+             public void actionPerformed(ActionEvent e) {
+                timer.stop();
+            }
+        });
+
+        buttonPanel.add(stopButton);
+
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+        timeFormatter = new DecimalFormat("00");
+
+        timer = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (centiseconds > 0) {
+                    centiseconds--;
+                } else {
+                    if (seconds == 0 && minutes == 0) {
+                        timer.stop();
+                    } else if (seconds > 0) {
+                        seconds--;
+                        centiseconds = 99;
+                    } else if (minutes > 0) {
+                        minutes--;
+                        seconds = 59;
+                        centiseconds = 99;
+                    }
+                }
+                timeLabel.setText(timeFormatter.format(minutes) + ":"
+                        + timeFormatter.format(seconds) + "."
+                        + timeFormatter.format(centiseconds));
+            }
+        });
+
+        timeLabel.setText(timeFormatter.format(minutes) + ":"
+                + timeFormatter.format(seconds) + "."
+                + timeFormatter.format(centiseconds));
+
+        add(panel);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setTitle("Contador");
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                }
+
+                new contador();
+            }
+        });
+    }
+}	
