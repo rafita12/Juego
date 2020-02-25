996a4094da6863a09c4074ec29e85229df22e3e3
FRAN <FRAN@172.16.17.67>
2020-02-25 14:28:07 +0100

    n

--- ClashRoyale/src/Jugadores/Jugador.java
+++ ClashRoyale/src/Jugadores/Jugador.java
@@ -1,11 +1,18 @@
 package Jugadores;
 
-public class Jugador {
+import java.util.Scanner;
 
+public class Jugador {
+	Scanner sc =new Scanner(System.in);
 	private boolean turno;
 	private int tiempoTurno;
 	
 	public void usaPersonaje() {
+		int array[]=new int[6];
+		for(int i=0;i<6;i++) {
+			System.out.println("Elija un personaje");
+			array[i]=sc.nextInt();
+		}
 		
 	}
 	
