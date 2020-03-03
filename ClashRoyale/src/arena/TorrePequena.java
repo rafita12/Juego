5ed7882d2cb81155e799207ca118ab6250f08588
usuario <usuario@usuario-OptiPlex-3050>
2020-03-03 16:43:27 +0100

    Atributos de la clase TorreGrande

---
+++ ClashRoyale/src/arena/TorrePequena.java
@@ -0,0 +1,30 @@
+package arena;
+
+public class TorrePequena {
+
+	private int vida;
+
+	public TorrePequena(int vida) {
+		
+		this.vida=vida;
+	}
+	
+	
+	
+	public int getVida() {
+		return vida;
+	}
+
+
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+
+
+	public void consulta() {
+		System.out.println();
+	}
+}
+
