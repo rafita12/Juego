f4429cf756c92ac009bc6643ba6658749e67a485
FRAN <FRAN@172.16.17.67>
2020-03-10 13:31:13 +0100

    Implementamos las clases Jugador1 y Jugador2 y modificamos la clase Mago

--- ClashRoyale/src/Jugadores/Jugador1.java
+++ ClashRoyale/src/Jugadores/Jugador1.java
@@ -2,19 +2,16 @@ package Jugadores;
 
 import java.util.Scanner;
 
-public class Jugador {
+public class Jugador1 {
 	Scanner sc =new Scanner(System.in);
 	private boolean turno;
 	private int tiempoTurno;
 	
 	public void usaPersonaje() {
-		int array[]=new int[6];
-		for(int i=0;i<6;i++) {
-			System.out.println("Elija un personaje");
-			array[i]=sc.nextInt();
+		
 		}
-	}
-	
-	
-	
 }
+	
+	
+	
+
