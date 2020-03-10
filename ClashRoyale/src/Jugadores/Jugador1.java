04663d61f4bf394c30008bb222f49c32f9667543
FRAN <FRAN@172.16.17.67>
2020-03-10 13:33:14 +0100

    cambios en jugador1

--- ClashRoyale/src/Jugadores/Jugador1.java
+++ ClashRoyale/src/Jugadores/Jugador1.java
@@ -3,11 +3,10 @@ package Jugadores;
 import java.util.Scanner;
 
 public class Jugador1 {
-	Scanner sc =new Scanner(System.in);
 	private boolean turno;
-	private int tiempoTurno;
 	
 	public void usaPersonaje() {
+		
 		
 		}
 }
