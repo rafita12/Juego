2a0e3a96795194133b10cc62d10aa2b18976a1e4
javierfrances <javierfrances@usuario-OptiPlex-3050>
2020-03-11 09:15:28 +0100

    Actualización final del día

--- ClashRoyale/src/Jugadores/Jugador1.java
+++ ClashRoyale/src/Jugadores/Jugador1.java
@@ -1,7 +1,5 @@
 package Jugadores;
 
-import java.util.Scanner;
-
 public class Jugador1 {
 	private boolean turno;
 	
@@ -9,6 +7,19 @@ public class Jugador1 {
 		
 		
 		}
+	public void elegirPersonajes() {
+		
+		String[] personajes = new String[] {"Arqueros","Bola de Fuego","Bombardero", "Caballero", "Catapulta", };
+		String[] strArray2 = {"A","B","C"}; 
+
+		//initialization after declaration
+		String[] strArray3 = new String[3];
+		strArray3[0] = "A";
+		strArray3[1] = "B";
+		strArray3[2] = "C";
+
+
+	}
 }
 	
 	
--- ClashRoyale/src/Personajes/Esqueletos.java
+++
@@ -1,7 +0,0 @@
-package Personajes;
-
-public class Esqueletos {
-
-	private int danoPersonaje = 25;
-
-}
