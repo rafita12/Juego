de2718c0134ddd0a30ef7d7fc1dc728eefe65854
javie <javie@192.168.56.1>
2020-03-28 19:44:10 +0100

    Actualizacion

--- ClashRoyale/src/Jugadores/Jugador1.java
+++ ClashRoyale/src/Jugadores/Jugador1.java
@@ -11,85 +11,57 @@ import java.util.Scanner;
 import Personajes.Personajes;
 
 public class Jugador1 {
-    private int turno;
-    private Personajes[]listaPersonajes;
-    private int nPersonajes;
+	private Personajes[] listaPersonajes;
+	private int nPersonajes;
 
+	public Jugador1() {
+		this.listaPersonajes = new Personajes[11];
+		this.nPersonajes = 0;
+	}
 
-public Jugador1(){
-    this.listaPersonajes=new Personajes[11];
-    this.nPersonajes=0;
- }
+	public void eliminaPersonaje(Personajes pj) {
 
-    public void eliminaPersonaje(Personajes pj) {
+		int inx, i;
+		inx = buscaPersonajes(pj);
 
-        int inx, i;
-        inx = buscaPersonajes(pj);
+		if (inx == -1) {
+			this.nPersonajes--;
+		}
+		nPersonajes--;
+		for (i = inx; i < nPersonajes; i++) {
+			listaPersonajes[i] = listaPersonajes[i + 1];
+		}
+		listaPersonajes[i] = null;
+	}
 
-        if (inx == -1) {
-            this.nPersonajes--;
-        }
-        nPersonajes--;
-        for (i = inx; i < nPersonajes; i++) {
-            listaPersonajes[i] = listaPersonajes[i + 1];
-        }
-        listaPersonajes[i] = null;
-    }
+	public int buscaPersonajes(Personajes pj) {
 
-    public int buscaPersonajes(Personajes pj) {
-    	
+		int inx = -1;
+		boolean enc = false;
+		for (int i = 0; !enc && i < listaPersonajes.length; i++) {
+			System.out
+					.println("Introduce el nombre del personaje que deseas elegir para tu equipo: " + listaPersonajes);
+			Scanner nj = new Scanner(System.in);
 
-        int inx = -1;
-        boolean enc = false;
-        for (int i = 0; !enc && i < listaPersonajes.length; i++) {
-        System.out.println("Introduce el nombre del personaje que deseas elegir para tu equipo: "+ listaPersonajes);
-        Scanner nj = new Scanner(System.in);
-        
-            if (listaPersonajes[i].equals(pj)) {
-                enc = true;
-                inx = i;
-            }
-        }
-        return inx;
-    }
+			if (listaPersonajes[i].equals(pj)) {
+				enc = true;
+				inx = i;
+			}
+		}
+		return inx;
+	}
 
-    
-public void turnoJugador(){
-    
-    if(turno==1) {
-    	System.out.println("Es el turno del Jugador 1");
-    	turno++;
-    }else if (turno==2) {
-    	System.out.println("Es el turno del jugador 2");
-    	turno--;
-    	}
-    
-    
-    
-    
-    }
-    
-    
-    
+	public void turnoJugador() {
+		int turno = (int)(Math.random()*1+1);		
+		
+		if (turno == 1) {
+			System.out.println("Es el turno del Jugador 1");
+			turno++;
+		} else if (turno == 2) {
+			System.out.println("Es el turno del jugador 2");
+			turno--;
+		}
+
+	}
+
 }
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
