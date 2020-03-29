095e9bb830272607f1f5b452b0edcaf79cf6a536
User <User@DESKTOP-GFK1O51.home>
2020-03-29 21:16:43 +0200

    Prueba de tablero

--- ClashRoyale/src/arena/Tablero.java
+++ ClashRoyale/src/arena/Tablero.java
@@ -3,10 +3,15 @@ package arena;
 public class Tablero {
 	
 	  private char [][] tablero ;
+	  private int col ;
+	  private int fil ;
+	  
 
 	  public Tablero(int tamano){
 	        
 	        this.tablero=new char [tamano][tamano];
+	        this.col=0;
+	        this.fil=0;
 	    }
 	    
 	    public void verTablero() {
@@ -14,16 +19,23 @@ public class Tablero {
 	        for (int col = 0; col < tablero[0].length * 2 + 2; col++)
 	            System.out.print("-");
 	        System.out.println();
+	        
 	        for (int fil = 0; fil < tablero.length; fil++) {
 	            System.out.print("|");
 	            for (int col = 0; col < tablero[fil].length; col++)
+	            	if (col>2 && col<6 && fil==1) {
+						System.out.print("--");
+					}
+	            	else
 	                        System.out.print("  ");
+	            
 	                    
 	            System.out.println("|");
 	            }
 	            for (int col = 0; col < tablero[0].length * 2 + 2; col++)
 	                System.out.print("-");
 	            System.out.println();
+	            
 
 	}
 	
